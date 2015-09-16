6d002331a387fdaf4215249a23544e89da761658
Cesar Valderrama <valderramago@gmail.com>
2015-09-16 00:27:16 -0500

    changed wickepdf exe path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,6 @@
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => Rails.root.join('bin', 'wkhtmltopdf').to_s
+  #:exe_path => Rails.root.join('bin', 'wkhtmltopdf').to_s
+  exe_path: '/usr/local/bin/wkhtmltopdf'
 }
